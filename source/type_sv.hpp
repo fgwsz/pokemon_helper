bad23730be6b92e807212d59ee3eca4e09739bf8
fgwsz <3191000458@qq.com>
2026-05-17 02:06:51 +0800

    删除不需要的api

--- source/type_sv.hpp
+++ source/type_sv.hpp
@@ -4,7 +4,6 @@
 
 #include"type.hpp"
 #include"enum_value.hpp"
-#include"what.hpp"
 
 namespace pokemon{
 
@@ -31,17 +30,6 @@ constexpr std::string_view type_sv_list[]={
 
 constexpr std::string_view type_to_type_sv(Type type)noexcept{
     return type_sv_list[enum_value(type)];
-}
-
-Type type_sv_to_type(std::string_view sv){
-    unsigned char index=0;
-    for(auto const& type_sv:type_sv_list){
-        if(sv==type_sv){
-            return static_cast<Type>(index);
-        }
-        ++index;
-    }
-    throw what("unknown type");
 }
 
 }//namespace pokemon
