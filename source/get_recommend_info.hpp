c253c91144e0d19e83c3e84ebd00dc86a5cf64a0
fgwsz <3191000458@qq.com>
2026-05-17 02:40:35 +0800

    修复推荐信息只有有效打击的盲点信息,1倍伤害也记入补充盲点

--- source/get_recommend_info.hpp
+++ source/get_recommend_info.hpp
@@ -32,11 +32,22 @@ bool operator>(RecommendInfoEntry const& lhs,RecommendInfoEntry const& rhs){
     if(lhs.coverage_gaps.size()!=rhs.coverage_gaps.size()){
         return lhs.coverage_gaps.size()>rhs.coverage_gaps.size();
     }
+    if(
+        lhs.coverage_gap_super_matchups.size()
+        !=rhs.coverage_gap_super_matchups.size()
+    ){
+        return lhs.coverage_gap_super_matchups.size()
+            >rhs.coverage_gap_super_matchups.size();
+    }
     float lhs_multiplier=0;
     float rhs_multiplier=0;
     auto lhs_itr=lhs.coverage_gap_super_matchups.cbegin();
     auto rhs_itr=rhs.coverage_gap_super_matchups.cbegin();
-    for(std::size_t index=0;index<lhs.coverage_gaps.size();++index){
+    for(
+        std::size_t index=0;
+        index<lhs.coverage_gap_super_matchups.size();
+        ++index
+    ){
         lhs_multiplier+=lhs_itr->multiplier;
         rhs_multiplier+=rhs_itr->multiplier;
         ++lhs_itr;
@@ -48,11 +59,18 @@ bool operator>(RecommendInfoEntry const& lhs,RecommendInfoEntry const& rhs){
     if(lhs.weaknesses.size()!=rhs.weaknesses.size()){
         return lhs.weaknesses.size()>rhs.weaknesses.size();
     }
+    if(
+        lhs.weakness_super_matchups.size()
+        !=rhs.weakness_super_matchups.size()
+    ){
+        return lhs.weakness_super_matchups.size()
+            >rhs.weakness_super_matchups.size();
+    }
     lhs_multiplier=0;
     rhs_multiplier=0;
     lhs_itr=lhs.weakness_super_matchups.cbegin();
     rhs_itr=rhs.weakness_super_matchups.cbegin();
-    for(std::size_t index=0;index<lhs.weaknesses.size();++index){
+    for(std::size_t index=0;index<lhs.weakness_super_matchups.size();++index){
         lhs_multiplier+=lhs_itr->multiplier;
         rhs_multiplier+=rhs_itr->multiplier;
         ++lhs_itr;
@@ -93,9 +111,11 @@ get_recommend_info(
                 matchup.attack_type,
                 *(matchup.defense_type.cbegin())
             );
-            if(matchup.multiplier>1){
+            if(matchup.multiplier>=1){
                 entry.coverage_gaps.emplace(def);
-                entry.coverage_gap_super_matchups.emplace_back(matchup);
+                if(matchup.multiplier>1){
+                    entry.coverage_gap_super_matchups.emplace_back(matchup);
+                }
             }
         }
         for(auto const& def:target_weaknesses){
@@ -106,9 +126,11 @@ get_recommend_info(
                 matchup.attack_type,
                 *(matchup.defense_type.cbegin())
             );
-            if(matchup.multiplier>1){
+            if(matchup.multiplier>=1){
                 entry.weaknesses.emplace(def);
-                entry.weakness_super_matchups.emplace_back(matchup);
+                if(matchup.multiplier>1){
+                    entry.weakness_super_matchups.emplace_back(matchup);
+                }
             }
         }
         if(!entry.coverage_gaps.empty()){
